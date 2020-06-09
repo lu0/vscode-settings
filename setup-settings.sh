52bacf5709e99fa41943a8111ab8da1ed437e35b
lu0 <lucero.alvaradorz@gmail.com>
2020-06-09 11:49:26 -0500

    relative paths and executable setup

--- setup-settings.sh
+++ setup-settings.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Setup symlinks of vscode settings
-rm -rf ~/.config/Code/User/keybindings.json && ln -s ~/dotfiles_linuxMint/config/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-rm -rf ~/.config/Code/User/settings.json && ln -s ~/dotfiles_linuxMint/config/vscode/settings_FHD.json ~/.config/Code/User/settings.json
-# rm -rf ~/.config/Code/User/settings.json && ln -s ~/dotfiles_linuxMint/config/vscode/settings_HD.json ~/.config/Code/User/settings.json
+rm -rf ~/.config/Code/User/keybindings.json && ln -sr json/keybindings.json ~/.config/Code/User/keybindings.json
+rm -rf ~/.config/Code/User/settings.json && ln -sr json/settings_FHD.json ~/.config/Code/User/settings.json
+# rm -rf ~/.config/Code/User/settings.json && ln -sr json/settings_HD.json ~/.config/Code/User/settings.json
 
 # Install extensions
-chmod +x ~/dotfiles_linuxMint/config/vscode/install_extensions.sh
-~/dotfiles_linuxMint/config/vscode/install_extensions.sh
+./install_extensions.sh
