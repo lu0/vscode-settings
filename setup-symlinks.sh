7579784c2eb2796075678304c0ebfd8fb5c3f058
lu0 <lucero.alvaradorz@gmail.com>
2020-08-05 22:51:24 -0500

    force symlinks

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Setup symlinks of vscode settings
-rm -rf ~/.config/Code/User/keybindings.json && ln -sr json/keybindings.json ~/.config/Code/User/keybindings.json
-rm -rf ~/.config/Code/User/settings.json && ln -sr json/settings_FHD.json ~/.config/Code/User/settings.json
-# rm -rf ~/.config/Code/User/settings.json && ln -sr settings_HD.json ~/.config/Code/User/settings.json
+ln -srf json/keybindings.json ~/.config/Code/User/keybindings.json
+ln -srf json/settings_FHD.json ~/.config/Code/User/settings.json
+# ln -srf settings_HD.json ~/.config/Code/User/settings.json
