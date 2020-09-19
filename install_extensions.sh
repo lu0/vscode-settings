94146792f327ee334f2bff619494121706911499
lu0 <lucero.alvaradorz@gmail.com>
2020-09-19 11:16:18 -0500

    kb: bookmarks, breakpoins and notifications

--- install_extensions.sh
+++ install_extensions.sh
@@ -2,35 +2,39 @@
 
 # Extensions for vscode
 
+code --install-extension alefragnani.bookmarks
 code --install-extension auchenberg.vscode-browser-preview
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension fivethree.vscode-ionic-snippets
-code --install-extension GabrielBB.vscode-lombok
-code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension Pivotal.vscode-boot-dev-pack
-code --install-extension Pivotal.vscode-concourse
 code --install-extension Pivotal.vscode-manifest-yaml
-code --install-extension Pivotal.vscode-spring-boot
-code --install-extension redhat.java
-code --install-extension redhat.vscode-yaml
 code --install-extension royaction.color-manager
-code --install-extension tht13.html-preview-vscode
-code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension Pivotal.vscode-concourse
+code --install-extension iocave.customize-ui
 code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-dependency
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension marcostazi.vs-code-drupal
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension tht13.html-preview-vscode
+code --install-extension fivethree.vscode-ionic-snippets
 code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-java-test
-# code --install-extension vscjava.vscode-maven
-code --install-extension vscjava.vscode-spring-boot-dashboard
-code --install-extension vscjava.vscode-spring-initializr
-code --install-extension vscodevim.vim
+code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
+code --install-extension redhat.java
+code --install-extension ritwickdey.liveserver
+code --install-extension GabrielBB.vscode-lombok
 code --install-extension shd101wyy.markdown-preview-enhanced
-code --install-extension iocave.customize-ui
-#code --disable-extension vscodevim.vim
+code --install-extension vscjava.vscode-maven
+code --install-extension iocave.monkey-patch
+code --install-extension esbenp.prettier-vscode
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension ms-python.python
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension vscjava.vscode-spring-boot-dashboard
+code --install-extension Pivotal.vscode-boot-dev-pack
+code --install-extension Pivotal.vscode-spring-boot
+code --install-extension vscjava.vscode-spring-initializr
+code --install-extension ms-mssql.mssql
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension redhat.vscode-yaml
+
 #code --disable-extension tht13.html-preview-vscode
 #code --disable-extension auchenberg.vscode-browser-preview
 
