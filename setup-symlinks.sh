6420a6499ba59226c9311dc953318f38d7ee6357
lu0 <lucero.alvaradorz@gmail.com>
2020-11-14 18:52:32 -0600

    Snippets folder. Python snippet for practice problems

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,3 +4,4 @@
 ln -srf json/keybindings.json ~/.config/Code/User/keybindings.json
 ln -srf json/settings_FHD.json ~/.config/Code/User/settings.json
 # ln -srf settings_HD.json ~/.config/Code/User/settings.json
+ln -srf json/snippets/* ~/.config/Code/User/snippets/
