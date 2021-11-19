2b91a202ecbffd580deabd3d73c6908c294dd25f
lu0 <lucero.alvaradorz@gmail.com>
2021-11-19 02:45:42 -0600

    Add a cumstom local theme.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Setup symlinks of vscode settings
+ln -srf theme-interplanetary-craft ~/.vscode/extensions/lu0.interplanetary-craft-dark-theme
 ln -srf json/keybindings.json ~/.config/Code/User/keybindings.json
 ln -srf json/settings_FHD.json ~/.config/Code/User/settings.json
 # ln -srf settings_HD.json ~/.config/Code/User/settings.json
