d915fd614e43a77256fe36fd7b9c6f7439ca9d2d
lu0 <me@lucero.codes>
2022-03-12 01:04:24 -0600

    Disable vim status bar colors to avoid lag

--- copy-configurations.sh
+++ copy-configurations.sh
@@ -3,9 +3,12 @@
 # Copy settings to the installation folder on Windows 10
 win_user_path=$(wslvar USERPROFILE) && \
 win_user=$(echo ${win_user_path##*\\}) && \
-win_config_dir=/mnt/c/Users/${win_user}/AppData/Roaming && \
-win_home_dir=/mnt/c/Users/${win_user} && \
-cp -r theme-interplanetary-craft ${win_home_dir}/.vscode/extensions/lu0.interplanetary-craft-dark-theme && \
-cp json/keybindings.json ${win_config_dir}/Code/User/keybindings.json && \
-cp json/settings_FHD.json ${win_config_dir}/Code/User/settings.json && \
-rm -rf ${win_config_dir}/Code/User/snippets && cp json/snippets ${win_config_dir}/Code/User/snippets
+win_config_dir=/mnt/c/Users/${win_user}/AppData/Roaming/Code/User
+win_home_dir=/mnt/c/Users/${win_user}
+win_vscode_config_dir=
+
+cp -r theme-interplanetary-craft ${win_home_dir}/.vscode/extensions/lu0.interplanetary-craft-dark-theme
+cp json/keybindings.json ${win_config_dir}/keybindings.json
+cp json/settings_FHD.json ${win_config_dir}/settings.json
+rm -rf ${win_config_dir}/snippets && \
+    cp json/snippets ${win_config_dir}/snippets
