7184bc89005b21eb9f444c3ca379757d6e9715b2
lu0 <me@lucero.codes>
2022-03-12 01:05:20 -0600

    Remove script not applicable for Windows

--- setup-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup symlinks of vscode settings
-ln -srf json/keybindings.json ~/.config/Code/User/keybindings.json
-ln -srf json/settings_FHD.json ~/.config/Code/User/settings.json
-# ln -srf json/settings_HD.json ~/.config/Code/User/settings.json
-
-# Install extensions
-./install_extensions.sh
