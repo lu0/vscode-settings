dfbed5f6f1a4831fb413ba95ee330c399375a1fd
lu0 <me@lucero.codes>
2022-03-12 01:08:23 -0600

    Remove support for HD screens

--- copy-configurations.sh
+++ copy-configurations.sh
@@ -9,6 +9,6 @@ win_vscode_config_dir=
 
 cp -r theme-interplanetary-craft ${win_home_dir}/.vscode/extensions/lu0.interplanetary-craft-dark-theme
 cp json/keybindings.json ${win_config_dir}/keybindings.json
-cp json/settings_FHD.json ${win_config_dir}/settings.json
+cp json/settings.json ${win_config_dir}/settings.json
 rm -rf ${win_config_dir}/snippets && \
     cp json/snippets ${win_config_dir}/snippets
